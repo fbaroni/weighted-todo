0a4fb488c85e84be67909ed165b8b1ee7602fe52
Federico Baroni <fbaroni@gmail.com>
2017-04-01 23:46:36 -0300

    se agregan day issues y day element

---
+++ app/Model/DailyElement.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Model;
+
+use Illuminate\Database\Eloquent\Model;
+
+class DailyElement extends Model
+{
+    protected $table = 'day_element';
+}
---
+++ app/Model/DailyIssue.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Model;
+
+use Illuminate\Database\Eloquent\Model;
+
+class DailyIssue extends Model
+{
+    protected $table = 'day_issue';
+}
