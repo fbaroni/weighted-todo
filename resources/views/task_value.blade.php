1fa028f3ec742fac39177eac1a10de2d5cfc07df
vagrant <fbaroni@gmail.com>
2018-01-15 21:41:56 +0000

    task valuation shown

--- resources/views/task_value.blade.php
+++ resources/views/task_value.blade.php
@@ -4,6 +4,6 @@
             <h3>{{ $title }}</h3>
         </div>
         <div class="col-lg-6 col-md-6 col-sm-6">
-            {{ $valuation }}
+            <h4>{{ number_format($valuation, 2) }} % </h4>
         </div>
     </div>
